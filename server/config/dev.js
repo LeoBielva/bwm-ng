1801b5211cff9cd62b0f57d77b3cb2ce4cf19e17
LeoBielva <leonardo.bielva@oliverwyman.com>
2019-09-23 19:22:17 -0500

    server added, routes & db connectiopn

---
+++ server/config/dev.js
@@ -0,0 +1,4 @@
+module.exports = {
+    DB_URI: "mongodb://localhost:27017/rentals"
+    // DB_URI: 'mongodb+srv://TestSubjectX23:Palitosdepan1@cluster0-iptmm.mongodb.net/test?retryWrites=true&w=majority'
+};
