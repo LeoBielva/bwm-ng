2fad69576e6f5d332768fb6691ba7f29e3ab387d
Mr.Arkham <36382349+LeoBielva@users.noreply.github.com>
2019-09-26 15:57:46 -0500

    Develop (#5)
    * google maps API added
    
    * google maps enhancement and fix

---
+++ src/app/common/map/map.module.ts
@@ -0,0 +1,30 @@
+import { NgModule } from '@angular/core';
+import { MapComponent } from './map.component';
+import { AgmCoreModule } from '@agm/core';
+import { CommonModule } from '@angular/common'; 
+import { NgPipesModule } from 'ngx-pipes';
+import { CamelizePipe } from 'ngx-pipes';
+
+import { MapService } from './map.service';
+
+@NgModule({
+  declarations: [
+      MapComponent
+  ],
+  exports:[
+      MapComponent
+    ],
+  imports: [
+    NgPipesModule,
+    AgmCoreModule.forRoot({
+        apiKey: 'GOOGLE_MAPS_API'
+    }),
+    CommonModule
+    ],
+    providers: [
+        MapService,
+        CamelizePipe
+    ]
+
+})
+export class MapModule { }
