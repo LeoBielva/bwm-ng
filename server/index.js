eba09399a1d2841e290f685962286f6e686e5789
Leo <leobielva93@hotmail.com>
2019-10-18 16:30:54 -0500

    Angular login pt1

--- server/index.js
+++ server/index.js
@@ -8,10 +8,27 @@ const FakeDb = require('./fake-db');
 const rentalRoutes = require('./routes/rentals');
 const userRoutes = require('./routes/users');
 
+const PORT = process.env.PORT || 3001;
+
+// const MongoClient = require('mongodb').MongoClient;
+// const uri = config.DB_URI;
+// MongoClient.connect(uri, function(err, client) {
+//     console.log(client.db);
+//     if(err) {
+//          console.log('Error occurred while connecting to MongoDB Atlas...\n',err);
+//     }
+//     console.log('Connected...');
+//   const collection = client.db("test").collection("test");
+//   // perform actions on the collection object
+//   client.close();
+// });
+
 mongoose.connect(config.DB_URI, { useUnifiedTopology: true, useNewUrlParser: true, useCreateIndex: true}).then(()=>{
     const fakeDb = new FakeDb();
-    fakeDb.seedDb();
+    // fakeDb.seedDb();
     console.log('Written');
+}).catch(err =>{
+    console.log(err)
 });
 
 const app = express();
@@ -24,19 +41,7 @@ app.get('/rental', function(req, res){
     res.json({'success': true})
 });
 
-const PORT = process.env.PORT || 3001;
-
 app.listen(PORT, function(){
     console.log("Node Server running! on", PORT);
 });
 
-// const MongoClient = require('mongoose').MongoClient;
-// const uri = "mongodb+srv://TestSubjectX23:Palitosdepan1@cluster0-iptmm.mongodb.net/test?retryWrites=true&w=majority";
-// const client = new MongoClient(uri, { useNewUrlParser: true });
-//                 client.connect(err => {
-//                 const collection = client.db("bwm-ng-dev").collection("Rentals");
-//                 // perform actions on the collection object
-//                 client.close();
-//             }
-//             );
-// };
